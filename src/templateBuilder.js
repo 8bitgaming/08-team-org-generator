00636239841876d5f21f3dbe1cc732ff09b0e1fa
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-02-12 01:31:46 -0600

    fixed template building for each employee, minor other fixes

--- src/templateBuilder.js
+++ src/templateBuilder.js
@@ -1,19 +1,53 @@
+//function to to iterate through all and create the cards for each employee
+const generateCards = (employees) => {
+  let allCards = []
+  employees.forEach(employee => {
 
+  //logic to check for the unique value for each role and apply it to the template
+    let specialField = ''
+    let specialFieldValue
+    if (employee.office) {
+      specialField = 'Office' 
+      specialFieldValue = employee.office
+    } else if (employee.github) {
+      specialField = 'GitHub'
+      specialFieldValue = `<a href="https://github.com/${employee.github}">${employee.github}</a/>`
+    } else {
+      specialField = 'School'
+      specialFieldValue = employee.school
+    }
+   
+    let card = `
+    <div class="w3-card-4 w3-col m5 l3 w3-margin">
+    <header class="w3-container w3-blue">
+      <h1>${employee.name}</h1>
+      <h3>${employee.role}</h3>
+    </header>
+    <div class="w3-container w3-padding-32">
+        <table class="w3-table w3-bordered">
+          <tr>
+            <th>ID:</th>
+            <td>${employee.id}</td>
+          </tr>
+          <tr>
+            <th>E-mail:</th>
+            <td><a href="mailto:${employee.email}">${employee.email}</a></td>
+          </tr>
+          <tr>
+            <th>${specialField}</th>
+            <td>${specialFieldValue}</td>
+          </tr>
+        </table>
+      </div>
+  </div>`
+  allCards.push(card)
+})
+
+return allCards.join('')
+}
+
+//initial call and boilderplate html
 const templateBuilder = (employees) => {
-// employees.forEach(employee => {
-//   if (employee.role === 'Manager') {
-//     createManagerCard(employee)
-//   } else if (employee.role === 'Engineer'){
-//     createEngineerCard(employee)
-//     } else {
-//       createInternCard(employee)
-//     }
-// }
-
-  // console.log("employees", employees)
-  //   [ managerName, managerId, managerEmail, managerRole, managerOffice] = employees.filter(employee => employee.role === 'Manager')
-  //   console.log("manager", manager)
-  // employees.forEach(employee => (employee.role === 'Manager') ? console.log(employee.name) : null)
     return `
     <!DOCTYPE html>
 <html>
@@ -28,33 +62,8 @@ const templateBuilder = (employees) => {
       <h1>My Team</h1>
     </header>
    <section class="employee-cards w3-padding-large w3-row ">
-       <!-- Example Card -->
-    <div class="w3-card-4 w3-col m4 l2 w3-margin">
-
-        <header class="w3-container w3-blue">
-          <h1>John Smith</h1>
-          <h3>Manager</h3>
-        </header>
-        
-        <div class="w3-container w3-padding-32">
-        
-            <table class="w3-table w3-bordered">
-              <tr>
-                <th>ID:</th>
-                <td>Smith</td>
-              </tr>
-              <tr>
-                <th>E-mail:</th>
-                <td>Smith</td>
-              </tr>
-              <tr>
-                <th>Office:</th>
-                <td>Smith</td>
-              </tr>
-            </table>
-          </div>
-        
-    </div>
+       <!-- Placeholder for cards -->
+       ${generateCards(employees)}
    </section> 
   </body>
 </html>
